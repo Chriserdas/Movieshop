2c92d347a176fcbf7b835fe2669291142e2fe772
Dionusia Psiri <66883669+Dionusia@users.noreply.github.com>
2022-01-10 12:53:25 +0200

    Revert "updates"
    This reverts commit c361eddf0c1a63b063379e286e3a332dc1ec6003.

--- trigger.sql
+++
@@ -1,7 +0,0 @@
-CREATE DEFINER=`root`@`localhost` TRIGGER `BLOCK_UPDATE` 
-BEFORE UPDATE ON `customer` FOR EACH ROW BEGIN
-IF(new.email != old.email) Then
- SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'Not allow to change your email';
- END IF;
-END
-
