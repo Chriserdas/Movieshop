75e8e439dff102f282fadeb587025717ffd7e58a
Dionusia Psiri <66883669+Dionusia@users.noreply.github.com>
2022-02-21 20:56:01 +0200

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -17,9 +17,10 @@ DELIMITER;
 /*INSERT INTO film_rental VALUES
 (17000, '2005-08-21 11:13:35', 23, 16);
 
-select * from log; */
+select * from log; 
 
-
+delete from film_rental where film_rental_id=17000;
+delete from log where username= 'SANDRA.MARTIN@sakilacustomer.org';*/
 
 DROP TRIGGER IF EXISTS after_update_film_log;
 DELIMITER$
@@ -70,6 +71,10 @@ FOR EACH ROW BEGIN
     insert into log values
     ((select email from customer where customer_id=new.customer_id), 'film_payment', 'insert', date_time, 'yes');
 END$
+
+/*INSERT INTO film_payment VALUES
+(16000,104,148,5.6,'2003-03-23');
+select * from log;*/
 DELIMITER;
 
 DROP TRIGGER IF EXISTS after_update_fpaym_log;
@@ -180,3 +185,113 @@ declare date_time datetime;
     ((select email from customer where customer_id=old.customer_id), 'episode_payment', 'delete', date_time, 'yes');
 END$
 DELIMITER;
+
+
+----trigger 4.3
+DROP TRIGGER IF EXISTS BLOCK_UPDATE;
+DELIMITER$
+CREATE DEFINER=`root`@`localhost` TRIGGER `BLOCK_UPDATE` 
+BEFORE UPDATE ON `customer` FOR EACH ROW BEGIN
+IF(new.email != old.email) Then
+ SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'Not allow to change your email';
+ END IF;
+ END$
+ DELIMITER;
+
+ /*UPDATE customer
+SET email = 'anna.WILLIAMS@sakilacustomer.org'
+WHERE email = 'LINDA.WILLIAMS@sakilacustomer.org'; */
+
+-----trigger 4.2 for film_rental
+DROP TRIGGER IF EXISTS film_amount;
+DELIMITER$
+CREATE DEFINER=`root`@`localhost` TRIGGER `film_amount` AFTER INSERT ON `film_rental` FOR EACH ROW BEGIN
+DECLARE c VARCHAR(10);
+	DECLARE countId INT;
+	DECLARE c1 INT;
+	DECLARE c2 INT;
+	DECLARE emailc VARCHAR(40);
+	DECLARE imerominia DATE;
+	DECLARE price DECIMAL(5,2);
+	SELECT email INTO emailc FROM customer WHERE customer_id = NEW.customer_id;
+	SELECT choice INTO c FROM customer where email like emailc;
+	SET imerominia = new.film_rental_date;
+	call set_amount(NEW.customer_id,'F', @price);
+	IF(c LIKE 'F') then
+	SELECT count(*) INTO countId
+	FROM film_rental
+	WHERE film_rental_date between CONCAT(imerominia," 00:00:00") and  CONCAT(imerominia," 23:59:59") AND customer_id = NEW.customer_id;
+
+	ELSEIF(c like 'FS') then 
+	SELECT count(*) INTO c1 FROM film_rental
+	WHERE film_rental_date between  CONCAT(imerominia," 00:00:00") and CONCAT(imerominia," 23:59:59") AND customer_id = NEW.customer_id;
+
+	SELECT count(*) INTO c2 FROM episode_rental
+	WHERE episode_rental_date between  CONCAT(imerominia," 00:00:00") and CONCAT(imerominia," 23:59:59") AND customer_id = NEW.customer_id;
+	SET countId = c1+c2;
+	END IF;
+    
+    IF(MOD(countId,3) =0) then
+		INSERT INTO film_payment VALUES
+	(NULL,NEW.customer_id,NEW.film_rental_id,@price/2,current_date());
+    else
+	 INSERT INTO film_payment VALUES
+     (NULL,NEW.customer_id,NEW.film_rental_id,@price,current_date());
+	end if;
+END$
+DELIMITER;
+
+/* INSERT INTO film_rental VALUES
+(16700,'2005-08-21', 691,33); 
+
+select * from film_payment;*/
+
+
+----trigger 4.2 for episode_rental
+DROP TRIGGER IF EXISTS episode_amount;
+DELIMITER$
+CREATE DEFINER=`root`@`localhost` TRIGGER `episode_amount` AFTER INSERT ON `episode_rental` FOR EACH ROW BEGIN
+    DECLARE c VARCHAR(10);
+	DECLARE countId INT;
+	DECLARE c1 INT;
+	DECLARE c2 INT;
+	DECLARE emailc VARCHAR(40);
+	DECLARE imerominia DATE;
+	DECLARE price DECIMAL(5,2);
+	SELECT email INTO emailc FROM customer WHERE customer_id = NEW.customer_id;
+	SELECT choice INTO c FROM customer where email like emailc;
+	SET imerominia = new.episode_rental_date;
+	call set_amount(NEW.customer_id,'S', @price);
+	IF(c LIKE 'S') then
+	SELECT count(*) INTO countId
+	FROM episode_rental
+	WHERE episode_rental_date between CONCAT(imerominia," 00:00:00") and  CONCAT(imerominia," 23:59:59") AND customer_id = NEW.customer_id;
+
+	ELSEIF(c like 'FS') then 
+	SELECT count(*) INTO c1 FROM film_rental
+	WHERE film_rental_date between  CONCAT(imerominia," 00:00:00") and CONCAT(imerominia," 23:59:59") AND customer_id = NEW.customer_id;
+
+	SELECT count(*) INTO c2 FROM episode_rental
+	WHERE episode_rental_date between  CONCAT(imerominia," 00:00:00") and CONCAT(imerominia," 23:59:59") AND customer_id = NEW.customer_id;
+	SET countId = c1+c2;
+	END IF;
+    
+    IF(MOD(countId,3) =0) then
+		INSERT INTO episode_payment VALUES
+	(NULL,NEW.customer_id,NEW.episode_rental_id,@price/2,current_date());
+	else
+	 INSERT INTO episode_payment VALUES
+     (NULL,NEW.customer_id,NEW.episode_rental_id,@price,current_date());
+    end if;
+    
+END$
+DELIMITER;
+
+/*INSERT INTO episode_rental VALUES
+(15500, '2003-02-13',40,16); gia na deiksw ekpwsh
+
+INSERT INTO episode_rental VALUES
+(15700, '2015-02-20',37,477); gia na deiksw oti mpainei kanonikh timh
+
+
+select * from episode_payment; */
